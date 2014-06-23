5f20b7dfc33913a217515be36d34a65e20cf42cb
Anders Fisher <atleastimtrying@gmail.com>
2014-06-23 12:05:17 +0100

    results stuff

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,5 +5,53 @@ class MainController < ApplicationController
 
   def results
     #this method returns a grouped collection of jobs to the collection
+    @results = {
+      '39.828 -104.983' => [
+        {
+          lat: 39.828,
+          lon: -104.983,
+          title: 'Teacher needed',
+          summary: 'Please view the details below of our Primary Teacher job',
+          company: 'CareerBuilder UK'
+        },
+        {
+          lat: 39.828,
+          lon: -104.983,
+          title: 'Teacher Job available',
+          summary: 'Please view the details below of our Highschool teacher job',
+          company: 'Mr Smith Jobs'
+        },
+        {
+          lat: 39.828,
+          lon: -104.983,
+          title: 'Teacher job',
+          summary: 'There is a summary here',
+          company: 'A Job search'
+        }
+      ],
+      '38.710 -102.650' => [
+        {
+          lat: 38.710,
+          lon: -102.650,
+          title: 'Teacher thing',
+          summary: 'Please view the details below of our Primary Teacher job',
+          company: 'CareerBuilder UK'
+        },
+        {
+          lat: 38.710,
+          lon: -102.650,
+          title: 'Teacher availability',
+          summary: 'Please view the details below of our Highschool teacher job',
+          company: 'Mr Smith Jobs'
+        },
+        {
+          lat: 38.710,
+          lon: -102.650,
+          title: 'Teacher option',
+          summary: 'There is a summary here',
+          company: 'A Job search'
+        }
+      ]
+    }
   end
 end
