90eb0abc4c9ad0398b41d0ff97b7b9aa1030af72
Max Shelley <max@measuredbrilliance.com>
2014-06-23 14:46:54 +0100

    Added view stuff.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,9 +6,13 @@ class MainController < ApplicationController
   def results
     #this method returns a grouped collection of jobs to the collection
     
-    @keyword = results_params[:keyword]
     @location_from = results_params[:location_from]
     @location_to = results_params[:location_to]
+
+    @location_from_origin = Job.geocode("#{@location_from}, UK")
+    @location_to_origin = Job.geocode("#{@location_to}, UK")
+
+    @keyword = results_params[:keyword]
     @results = Job.grouped_by_lat_lng(@keyword)
 
   end
